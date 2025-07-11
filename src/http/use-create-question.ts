720daef4abcdf1eaeb0920e31e6f8c0f96725c00
Alessandro Bruno Lima <alessandrobrunolima@gmail.com>
2025-07-10 21:56:17 -0300

    feat: Adiciona funcionalidade de gravação de áudio em salas, incluindo novos componentes para exibição de perguntas e integração com a API para criação e recuperação de perguntas. Atualiza dependências e implementa lógica de controle de estado para gravação.

---
+++ src/http/use-create-question.ts
@@ -0,0 +1,85 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import type { GetRoomQuestionsResponse } from "./use-room-questions";
+
+type CreateQuestionRequest = {
+  question: string;
+};
+
+type CreateQuestionResponse = {
+  questionId: string;
+  answer: string | null;
+};
+
+export function useCreateQuestion(roomId: string) {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: CreateQuestionRequest) => {
+      const response = await fetch(`http://localhost:3333/rooms/${roomId}/questions`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result: CreateQuestionResponse = await response.json();
+      return result;
+    },
+    onMutate({question}){
+
+      const questions = queryClient.getQueryData<GetRoomQuestionsResponse>(['get-room-questions', roomId]);
+      const questionsArray = questions ?? [];
+
+      const newQuestion = {
+        id: crypto.randomUUID(),
+        question,
+        answer: null,
+        createdAt: new Date().toISOString(),
+        isGeneratingAnswer: true
+      }
+
+      queryClient.setQueryData<GetRoomQuestionsResponse>(['get-room-questions', roomId], [
+        newQuestion, 
+        ...questionsArray
+      ]);
+
+      return {newQuestion, questions}
+
+    },
+    onSuccess(data, _variables, context){
+      queryClient.setQueryData<GetRoomQuestionsResponse>(['get-room-questions', roomId], 
+        questions => {
+          if(!questions){
+            return questions;
+          }
+
+          if(!context.newQuestion){
+            return questions;
+          }
+
+          return questions.map(question => {
+            if(question.id === context.newQuestion.id){
+              return {
+                ...context.newQuestion,
+                id: data.questionId,
+                answer: data.answer,
+                isGeneratingAnswer: false
+              }
+            }
+
+            return question;
+          });
+
+        }
+      );
+    },
+    onError(_error, _variables, context){
+      if(context?.questions){
+        queryClient.setQueryData<GetRoomQuestionsResponse>(['get-room-questions', roomId], context.questions);
+      }
+      
+    }
+    // onSuccess: () => {
+    //   queryClient.invalidateQueries({ queryKey: ['get-room-questions', roomId] });
+    // }
+  });
+}
